3f36b67fa57a1169ae2dadaae825086ddbe70c84
너굴고양이 <102637176+reproduce0529@users.noreply.github.com>
2023-04-13 16:32:52 +0900

    Delete doum directory

--- doum/js/chat.js
+++
@@ -1,5 +0,0 @@
-window.onload = function() {
-    document.querySelector(".top_title").innerHTML = localStorage.getItem("username") + " 님";
-    $(document.querySelector(".chatBox")).scrollTop($(document.querySelector(".chatBox")).height());
-
-}
